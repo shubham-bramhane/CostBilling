0e55954482e07d908ba95be3efa89c12083be9b7
shubham bramhane <59164754+shubham-bramhane@users.noreply.github.com>
2023-09-01 10:45:30 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,7 @@
                     @endauth
                 </div>
             @endif
+            
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
